a1bf0ee2a53346ffb26b9e2fb1d0e12a425c6ec8
Amir <amir@192.168.1.3>
2024-05-13 21:54:50 +0300

    added task one

---
+++ task one .py
@@ -0,0 +1,40 @@
+#print('hello')
+#print('hello','omar','mohamed')
+#input('enter your name')
+#print('hello',input('enter your name'))
+#name='mona'
+#name2=67
+#name_89=89
+#print('hello',name_89)
+#name='mona shabaan'
+#print(type(name))
+#y=int(input('enter your gender'))
+name='mona'
+age=35
+salary=50004.65
+is_enough=True
+#print('hello',name, 'my age is',age,'iam engineer take monthly',salary,'and thats enought to me is not it ',is_enough)
+
+#print(f'hello',{name})
+print('hello',{name},"your age is",{age})
+print('welcome sir',{name},'your expected sallry is ',{salary},'we think that is satisfyed')
+
+#num=15
+#num2=str(num)
+#print(type(num2))
+#num1=int(input('enter the number 1'))
+#num6=int(input('enter second number'))
+#print(num1+num6)
+#print(num1*num6)
+#print(num1-num6)
+#print(num1**num6)
+#print(num1%num6)
+#x=10
+#if x==10 :
+print(10>=15)
+    #print( "mona habibty")
+#else :
+#    print("ahmed habiby")
+
+name="mona"
+print(True) if name=="mona" else print(False)
