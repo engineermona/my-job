a71511604f83cec928419e270185db051741d7cc
Amir <amir@Amirs-MacBook-Air.local>
2024-05-18 19:54:27 +0300

    Added part3

---
+++ task1 part 3.py
@@ -0,0 +1,34 @@
+#question 5 
+#number1=float(input('enter the first num'))
+#number2= float(input("enter the second num"))
+#if number1>number2:
+ #   print(number1)
+#else:
+#   print(number2)
+# question 5 short if 
+#number1=float(input('enter the first num'))
+
+#number2=float(input("enter the second num"))
+#print("number1 is bigger")if number1>number2 else print("number2 is more")
+#question 9
+#number1=float(input("enter the first"))
+#number2=float(input("enter the second"))
+#number3=float(input("enter the third"))
+#if number1>number2 and number1>number3:
+ #   print(number1)
+#elif number2>number1 and number2>number3:
+#    print(mumber2)
+#else :
+#    print(number3)
+#task1 part1 creat a list
+#number=[1,2,3,4,5] 
+#number2=(10,11,12,13,14,15)
+#convert from list to tuple
+#creat adict of 3 values
+#student1=['name':'mona' ,'age':"34" ,"sallary":'76533655']
+#last question in part 1
+numbers=[2,4,6,8,10]
+print("right") if 4 in numbers else print()
+print('good') if 4 and 6 in numbers else print()
+print("any one") if 3 or 6 in numbers else print()
+print("any of them") if 2 and 4 and 5 in numbers else print ()
