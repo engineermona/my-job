0b54879b8c1ca0a000c7a5f10a279645e4ad6f10
Amir <amir@Amirs-MacBook-Air.local>
2024-05-25 19:55:09 +0300

    added lec 4

---
+++ lec4 .py
@@ -0,0 +1,15 @@
+#name="omar"
+#print(type(name))
+#print(name.capitalize())
+#print(name.upper())
+#print(name.lower())
+#print(name.islower())
+#print(name.count("m"))
+#print(name.index("o"))
+names="omar Mohamed"
+#print(names.index("m"))
+#name=(input("enter your name"))
+#print(name.strip()) 
+#print(names.split())
+#print(names.split("r"))
+
