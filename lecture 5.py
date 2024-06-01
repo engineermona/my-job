55f260a41d40c9bf1fcc0d67ec085cb2523eb5f0
Amir <amir@192.168.1.4>
2024-06-01 11:03:03 +0300

    added sheet 3

--- lecture 5.py
+++ lecture 5.py
@@ -1,6 +1,49 @@
-students=int(input("enter number of students"))
-new_student=[]
-for i in range(1,students+1) :
-    student=input("enter the name").strip().capitalize()
-    new_student.append(student)
-print(new_student)
+#question 1 in sheet 3 
+#numbers=[12,75,150,180,145,525,50]
+#for numb in numbers:
+    #if numb%5==0:
+     #   print(numb)
+#numbers=[12,75,150,180,145,525,50]
+#for num in numbers:
+ #   if num>150:
+  #      continue
+   # print(num)
+#for num in numbers:
+ #   if num>500:
+  #      break
+   # print(num)
+   #question 4 
+#range(-10,0,1)
+#for i in range(-10,0,1):
+ #   print(i)
+#question 5 
+#num=int(input("enter the number"))
+#for i in range(0,11,1):
+ #   print(num*i)
+ #question 3
+#list1=[10,20,30,40,50]
+#new_list=(list1[ : :-1])
+#for i in new_list:
+ #   print(i)
+#question 6
+list=[0,1,1,2]
+i=11
+while i<len(list): 
+   number1=list[-1]
+   number2=list[-2]
+   new_number=number1+number2
+   list.append(new_number)
+   print(list)
+
+
+
+
+
+
+
+
+
+
+    
+
+       
