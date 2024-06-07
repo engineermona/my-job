581dd245debd73f13e1ef9bf58204b73877574d8
Amir <amir@192.168.1.4>
2024-06-07 23:54:55 +0300

    added quiz

--- lecture 5.py
+++ lecture 5.py
@@ -21,22 +21,42 @@
 #for i in range(0,11,1):
  #   print(num*i)
  #question 3
-#list1=[10,20,30,40,50]
-#new_list=(list1[ : :-1])
-#for i in new_list:
- #   print(i)
+list1=[10,20,30,40,50]
+new_list=(list1[ : :-1])
+for i in new_list:
+    print(i)
 #question 6
 list=[0,1,1,2]
+new_list=[]
 i=11
-while i<len(list): 
+while i<len(list):
    number1=list[-1]
    number2=list[-2]
    new_number=number1+number2
    list.append(new_number)
-   print(list)
+   new_list.append(list)
+for list in new_list:
+   print(new_list)
+
+#number=str(input("enter the number"))
+
+#list1=[10,20,30,40,50]
+#for i in (list1[ : :-1]):
+ #   print(i)
+#question 6
 
 
+#new_list=[]
+#for i in range(10,60,10):
+   #newlist.append(i)
+   #print(new_list)[-1]
+ #  q2
+number=int(input("enter the number"))
+modify=str(number)
+while i in range(0,len(modify)):
+   print(i)
 
+   
 
 
 
